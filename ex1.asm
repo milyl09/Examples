098c440ecbd67559de54f74d58fdaeb5dd8bd1a0
milyl09 <emilyleon.09@gmail.com>
2025-05-13 10:42:38 -0700

    yes

--- ex1.asm
+++ ex1.asm
@@ -10,23 +10,23 @@ f: .word 0   # We'll store the result here
 .globl main
 main:
  # Load values from memory into registers
-    lw $t1, g     # $t1 = g
-    lw $t2, h     # $t2 = h
-    lw $t3, i     # $t3 = i
-    lw $t4, j     # $t4 = j
+   lw $t1, g     # $t1 = g
+   lw $t2, h     # $t2 = h
+   lw $t3, i     # $t3 = i
+   lw $t4, j     # $t4 = j
 
  # if (i == j)
-    bne $t3, $t4, false  
+   bne $t3, $t4, false  
  # f = g + h
-    add $t0, $t1, $t2       
-    j endif 
+   add $t0, $t1, $t2       
+   j endif 
 false: 
-    sub $t0, $t1, $t2 
+   sub $t0, $t1, $t2 
 endif:   
  # Store result back to memory      
-    sw $t0, f
+   sw $t0, f
 # Exit program
-    li $v0, 10
-    syscall
+   li $v0, 10
+   syscall
 
 
