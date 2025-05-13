0824422f73aeea9dfb10332993e68463c57da831
milyl09 <emilyleon.09@gmail.com>
2025-05-13 10:46:54 -0700

    bruh

--- ex1.asm
+++ ex1.asm
@@ -1,32 +1,29 @@
 .data
-# Declare variables in memory
 g: .word 7
 h: .word 3
 i: .word 5
 j: .word 5
-f: .word 0   # We'll store the result here
+f: .word 0
 
 .text
 .globl main
 main:
- # Load values from memory into registers
-   lw $t1, g     # $t1 = g
-   lw $t2, h     # $t2 = h
-   lw $t3, i     # $t3 = i
-   lw $t4, j     # $t4 = j
+    lw $t1, g         # Load g into $t1
+    lw $t2, h         # Load h into $t2
+    lw $t3, i         # Load i into $t3
+    lw $t4, j         # Load j into $t4
 
- # if (i == j)
-   bne $t3, $t4, false  
- # f = g + h
-   add $t0, $t1, $t2       
-   j endif 
-false: 
-   sub $t0, $t1, $t2 
-endif:   
- # Store result back to memory      
-   sw $t0, f
-# Exit program
-   li $v0, 10
-   syscall
+    bne $t3, $t4, else_part  # If i != j, go to else
 
+    add $t0, $t1, $t2        # f = g + h
+    j end_if                 # Skip else part
+
+else_part:
+    sub $t0, $t1, $t2        # f = g - h
+
+end_if:
+    sw $t0, f                # Store result in f
+
+    li $v0, 10               # Exit program
+    syscall
 
